8f89f586c64848521d335188a4af9ad481f2e72b
Pedro Herrera <pedroherlop@hotmail.com>
2022-05-15 20:40:24 +0200

    List elements saved in the room

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import Pad from '../Pad/Pad';
+import { useState, useEffect } from 'react';
 
 function ItemList(props) {
+	const [itemMap, setItemMap] = useState(new Map());
+
 	function renderList() {
-		return props.itemList.map((item, index) => 
-			<Pad key={index} text={item.text} sound={item.file}></Pad>
-		);
+		return [...itemMap.keys()].map(key => {
+			var padItem = itemMap.get(key);
+			return <Pad key={key} text={padItem.text} sound={padItem.file}></Pad>
+		});
 	}
+
+
+	useEffect(() => {
+		var firstRender = true;
+		
+		props.itemsNode
+			.map()
+			.on((data, key) => {
+				if(data !== null && !firstRender) {
+					setItemMap(prevState => new Map(prevState).set(key, data));
+				}
+				return data;
+		});
+		firstRender = false;
+	}, [props.itemsNode]);
 
 	return (
 	<div>
