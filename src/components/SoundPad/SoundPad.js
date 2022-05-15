f0a71b7568f3fb29553c9ec77f2dffb2578b167d
Pedro Herrera <pedroherlop@hotmail.com>
2022-05-15 21:45:00 +0200

    Avoid storage warning messages and increase load performance

--- src/components/SoundPad/SoundPad.js
+++ src/components/SoundPad/SoundPad.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ItemInput from '../ItemInput/ItemInput';
 import ItemList from '../ItemList/ItemList';
+import Gun from 'gun';
+import 'gun/lib/radix';
+import 'gun/lib/radisk';
+import 'gun/lib/store';
+import 'gun/lib/rindexed';
 
 function SoundPad() {
-	const GUN = require('gun');
-	var gun = GUN('https://gun-manhattan.herokuapp.com/gun');
-	var roomNode = gun.get('AAAC');
+	var opt = {
+		peers: 'https://gun-manhattan.herokuapp.com/gun',
+		localStorage: false
+	};
+
+	var gun = Gun(opt);
 	var soundPadNode = roomNode.get('soundPad');
 	var itemsNode = soundPadNode.get('items');
 
