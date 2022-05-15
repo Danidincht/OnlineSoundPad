5cdd0ffe0f98e5a1fb5703dcf09337c16cdc32ec
Pedro Herrera <pedroherlop@hotmail.com>
2022-05-15 20:30:24 +0200

    Initialize GunJS with dev room passing node to items

--- src/components/SoundPad/SoundPad.js
+++ src/components/SoundPad/SoundPad.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ItemInput from '../ItemInput/ItemInput';
 import ItemList from '../ItemList/ItemList';
+
 function SoundPad() {
 	const GUN = require('gun');
+	var gun = GUN('https://gun-manhattan.herokuapp.com/gun');
+	var roomNode = gun.get('AAAC');
+	var soundPadNode = roomNode.get('soundPad');
+	var itemsNode = soundPadNode.get('items');
 
 	return (
 	<div>
-		<ItemInput></ItemInput>
-		<ItemList itemList={state.itemList}></ItemList>
+		<ItemInput itemsNode={itemsNode}></ItemInput>
+		<ItemList itemsNode={itemsNode}></ItemList>
 	</div>
 )};
 
