84088a83bdff1fada58845afb78c4f3c10653444
Pedro Herrera <pedroherlop@hotmail.com>
2022-05-18 21:19:06 +0200

    Add db update on pad isPlaying property on play (true) and pause (false)

--- src/components/Pad/Pad.js
+++ src/components/Pad/Pad.js
@@ -13,12 +13,25 @@ function Pad(props) {
 			file = data.file;
 			isPlaying = data.isPlaying;
 		});
+
+	function onPlay(e) {
+		e.preventDefault();
+		props.padNode.get(props.id).put({audio: { isPlaying: true }})
+	}
+
+	function onPause(e) {
+		e.preventDefault();
+		props.padNode.get(props.id).put({audio: { isPlaying: false }})
+	}
+
 	return (
 	<span>
 		<audio 
 			controls
 			ref={audioElementRef}
 			src={file}
+			onPlay={onPlay}
+			onPause={onPause}
 		></audio>
 	</span>
 )};
