e1eee286a914ac35d5129b3f2ce3e2118f94e51b
Pedro Herrera <pedroherlop@hotmail.com>
2023-04-27 20:34:49 +0200

    Fixed test

--- src/App/__test__/App.test.js
+++ src/App/__test__/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from '../App';
 
-test('renders learn react link', () => {
+test('renders basic main window text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Main window/i);
   expect(linkElement).toBeInTheDocument();
 });
