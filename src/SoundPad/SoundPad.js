9f335bea9eb18f5e007c104a19e2fb5d1a674d01
Pedro Herrera <pedroherlop@hotmail.com>
2023-04-28 18:57:52 +0200

    Make SoundPad component render a list of PadItems dinamically

--- src/SoundPad/SoundPad.js
+++ src/SoundPad/SoundPad.js
@@ -1,11 +1,31 @@
 import './SoundPad.css';
+import PadItem from '../PadItem/PadItem';
 
 function SoundPad() {
-  return (
-    <div>
-      Main window
-    </div>
-  );
+	const items = [
+		createPadItemContent('PadItem1', 'https://www.sousound.com/music/healing/healing_01.mp3'),
+		createPadItemContent('PadItem2', 'https://www.sousound.com/music/healing/healing_02.mp3')
+	];
+	return (
+		<div>
+			Sound Pad Online
+			{
+				items.map((item) => (
+					<PadItem 
+						title={item.title}
+						audio={item.audio}
+					/>
+				))
+			}
+		</div>
+	);
+}
+
+function createPadItemContent(title, audio) {
+	return {
+		title,
+		audio
+	}
 }
 
 export default SoundPad;
