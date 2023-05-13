ca96b58cbe6298d804a51cef9d5b98c6be3b8672
Pedro Herrera <pedroherlop@hotmail.com>
2023-05-13 19:13:19 +0200

    Fixed ESLint config and added rules

--- eslint.config.js
+++ eslint.config.js
@@ -10,11 +10,18 @@ module.exports = [
 	{
 		files: ['src/**/*.{js,jsx,mjs,cjs,ts,tsx}'],
 		rules: {
+			// Extension rules
 			...recommended.rules,
 			quotes: ['error', 'single'],
+
+			// Plugins rules 
 			'jsx-quotes': ['error', 'prefer-single'],
 			'react/jsx-uses-react': 'error',
 			'react/jsx-uses-vars': 'error',
+
+			// Added rules
+			'semi-style': ['error', 'last'],
+			'semi': ['error', 'always', {}]
 		},
 		languageOptions: {
 			parserOptions: {
