2aa1dc1b6e059813836907f16d242ae18bdc176e
Pedro Herrera <pedroherlop@hotmail.com>
2023-05-18 19:44:47 +0200

    Convert component to use function structure and add tests

--- src/PadEditor/PadEditor.js
+++ src/PadEditor/PadEditor.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './PadEditor.css';
 
-const PadEditor = () => (
-	<div className='PadEditor'>
-		<form>
-			<input name='title' type='text' placeholder='Pad title' onChange={handleTitleChange}/>
-		</form>
-		PadEditor Component
-	</div>
-);
+function PadEditor () {
+	return (
+		<div className='PadEditor'>
+			<form>
+				<input name='title' type='text' placeholder='Pad title' value={titleInputValue}/>
+			</form>
+			PadEditor Component
+		</div>
+	);
+}
 
 export default PadEditor;
--- src/PadEditor/__test__/PadEditor.test.js
+++ src/PadEditor/__test__/PadEditor.test.js
@@ -3,15 +3,35 @@ import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import PadEditor from '../PadEditor';
 
-describe('PadEditor', () => {
-  it('it should render the PadEditor component', () => {
-	// Given
-	const component = (<PadEditor />);
-    
-	// When
-	const page = render(component).baseElement;
+const selectors = {
+	titleInput: 'input[name=title]'
+};
 
-	// Then
-	expect(page).toMatchSnapshot();
-  });
+describe('PadEditor', () => {
+	it('it should render the PadEditor component', () => {
+		// Given
+		const component = (<PadEditor />);
+
+		// When
+		const page = render(component).baseElement;
+
+		// Then
+		expect(page).toMatchSnapshot();
+	});
+
+	describe('Title input', () => {
+		let titleInput;
+		beforeEach(() => {
+			const {container} = render(<PadEditor />);
+			titleInput = container.querySelector(selectors.titleInput);
+		});
+		it('Has expected properties', () => {
+			// Given - When - Then
+			expect(titleInput.getAttribute('placeholder')).toEqual('Pad title');
+			expect(titleInput.getAttribute('type')).toEqual('text');
+			expect(titleInput.getAttribute('name')).toEqual('title');
+			expect(titleInput.getAttribute('value')).toEqual('');
+		});
+
+	});
 });
