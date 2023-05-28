3f6fce3e3697ee1f5645b9fbb5b0fbbb30a6f399
Pedro Herrera <pedroherlop@hotmail.com>
2023-05-28 13:38:45 +0200

    Add GunJSHelper upper section on test file

--- src/SoundPad/__test__/GunJSHelper.test.js
+++ src/SoundPad/__test__/GunJSHelper.test.js
@@ -5,37 +5,39 @@ import Gun from 'gun';
 
 afterEach(jest.resetModules);
 
-describe('getGunInstance', () => {
-	it('creates a GunJS instance', () => {
-		// Given - When
-		getGunInstance();
-		
-		// Then
-		expect(Gun).toBeCalledTimes(1);
-		expect(Gun).toBeCalledWith(expect.objectContaining({
-			peers: 'https://gun-manhattan.herokuapp.com/gun',
-			localStorage: false
-		}));
+describe('GunJSHelper', () => {
+	describe('getGunInstance', () => {
+		it('creates a GunJS instance', () => {
+			// Given - When
+			getGunInstance();
+			
+			// Then
+			expect(Gun).toBeCalledTimes(1);
+			expect(Gun).toBeCalledWith(expect.objectContaining({
+				peers: 'https://gun-manhattan.herokuapp.com/gun',
+				localStorage: false
+			}));
+		});
 	});
-});
 
-describe('getItemsNode', () => {
-	it('gets items node of current room', () => {
-		// Given
-		var gunMock = {
-			get: jest.fn(x => gunMock)
-		},
-		roomName = 'roomName';
-		Gun.mockReturnValue(gunMock);
+	describe('getItemsNode', () => {
+		it('gets items node of current room', () => {
+			// Given
+			var gunMock = {
+				get: jest.fn(x => gunMock)
+			},
+			roomName = 'roomName';
+			Gun.mockReturnValue(gunMock);
 
-		// When
-		getItemsNode(roomName);
+			// When
+			getItemsNode(roomName);
 
-		// Then
-		expect(Gun).toBeCalledTimes(1);
-		expect(gunMock.get).toBeCalledTimes(3);
-		expect(gunMock.get.mock.calls[0][0]).toBe(roomName);
-		expect(gunMock.get.mock.calls[1][0]).toBe('soundPad');
-		expect(gunMock.get.mock.calls[2][0]).toBe('items');
+			// Then
+			expect(Gun).toBeCalledTimes(1);
+			expect(gunMock.get).toBeCalledTimes(3);
+			expect(gunMock.get.mock.calls[0][0]).toBe(roomName);
+			expect(gunMock.get.mock.calls[1][0]).toBe('soundPad');
+			expect(gunMock.get.mock.calls[2][0]).toBe('items');
+		});
 	});
 });
